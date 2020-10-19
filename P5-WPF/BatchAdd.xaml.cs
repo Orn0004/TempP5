8809fb68b37bf4cdd6cdc5ae212f16ebcf261959
Orn Blaevarr Svansson <OrnApache@live.com>
2020-10-20 01:09:56 +0200

    DB connect. Virker ikke endnu

--- P5-WPF/BatchAdd.xaml.cs
+++ P5-WPF/BatchAdd.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using MySql.Data;
 
 namespace P5_WPF
 {
@@ -26,6 +30,27 @@ namespace P5_WPF
         private void addToDB(object sender, RoutedEventArgs e)
         {
 
+
+            string CS = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            using (MySqlConnection con = new MySqlConnection(CS))
+                try
+                {
+                    {
+
+                        MySqlCommand cmd = new MySqlCommand("INSERT INTO batch (BatchNr, Temp, Shelf) Values (@BatchNr, @Temp, @Shelf) ");
+                        cmd.Parameters.AddWithValue("@BatchNr", BatchNr.Text);
+                        cmd.Parameters.AddWithValue("@Temp", Temp.Text);
+                        cmd.Parameters.AddWithValue("@Shelf", Shelf.Text);
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Cannot establish connection");
+                    MessageBox.Show(ex.Message);
+                }
         }
     }
 }
