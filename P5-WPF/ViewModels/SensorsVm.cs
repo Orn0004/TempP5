341cd3009e47a78cd8398b013a1583cf2e38b8ae
fonzen1 <48014544+fonzen1@users.noreply.github.com>
2020-12-01 19:51:59 +0100

    init

---
+++ P5-WPF/ViewModels/SensorsVm.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace P5_WPF.ViewModels
+{
+    class SensorsVm
+    {
+        public SensorsVm()
+        {
+            string CS = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            //ActiveSensorsInjection(CS);
+        }
+        //private void ActiveSensorsInjection(string CS)
+        //{
+        //    using (MySqlConnection connection = new MySqlConnection(CS))
+        //    {
+        //        try
+        //        {
+        //            string query = "SELECT ID from aktivesensorer";
+        //            MySqlDataAdapter da = new MySqlDataAdapter(query, connection);
+        //            connection.Open();
+        //            DataSet ds = new DataSet();
+        //            da.Fill(ds, "Sensor");
+        //            cmbTripName.DisplayMember = "ID";
+        //            cmbTripName.ValueMember = "ID";
+        //            cmbTripName.DataSource = ds.Tables["Sensor"];
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            // write exception info to log or anything else
+        //            MessageBox.Show("Error occured!");
+        //        }
+        //    }
+        //}
+    }
+}
+
